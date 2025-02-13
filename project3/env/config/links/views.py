3b978b37d39be266f29e388926909de678cee669
Piyush-daryapurkar <piyushdaryapurkar309@gmail.com>
2025-02-14 01:40:21 +0530

    add-links(data)

--- project3/env/config/links/views.py
+++ project3/env/config/links/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,HttpResponse,redirect,get_object_or_404
 from django.urls import reverse
 
 from .models import Link
+from .forms import LinkForm
 # Create your views here.
 def index(request):
     links=Link.objects.all()
@@ -16,3 +17,16 @@ def root_link(request ,link_slug):
 
     return redirect(link.url)
 
+def create_link(request):
+    if request.method == 'POST':
+        form = LinkForm(request.POST)
+        if form.is_valid():
+             form.save()
+             return redirect(reverse('home'))
+    else:
+           form = LinkForm()
+    context={
+        "form" : form
+    }
+ 
+    return render(request,'create.html',context)
